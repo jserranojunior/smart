95062fe6ed8d8a6cf3464586ac761002e109e1e1
Jorge Serrano Junior <jorgeserarnojunior@hotmail.com>
2018-03-23 17:10:19 -0300

    Campos Coren RH

--- resources/views/rh/editar.blade.php
+++ resources/views/rh/editar.blade.php
@@ -20,7 +20,8 @@
                     </div>
                     <div class="card-body">
                         <form action="#" class="">                      
-                            @foreach($dados as $item)                   
+                            @foreach($dados as $item)         
+                                      
                                 <table class="table table-sm table-align-left">
 
                                 <tr>                                    
@@ -220,7 +221,7 @@
                                 </tr> 
                                 <tr>
                                     <td>
-                                        <label for="nm_tit_eleitor">Tittulo Eleitor</label>                                    
+                                        <label for="nm_tit_eleitor">Titulo Eleitor</label>                                    
                                         </td>
                                         <td  colspan="">
                                         <input type="text" name="nm_tit_eleitor" value="{{$item->nm_tit_eleitor}}" class="form-control-sm form-control">
@@ -257,8 +258,156 @@
                                         <td  colspan="">
                                         <input type="text" name="cd_banco_conta" value="{{$item->cd_banco_conta}}" class="form-control-sm form-control">
                                     </td>                      
-                                </tr>  
-
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <label for="cd_sptrans">Cartão Sptrans</label>                                    
+                                        </td>
+                                        <td  colspan="">
+                                        <input type="text" name="cd_sptrans" value="{{$item->cd_sptrans}}" class="form-control-sm form-control">
+                                    </td>
+                                    <td>
+                                        <label for="cd_cmt_bom">Cartão Bom</label>                                    
+                                        </td>
+                                        <td  colspan="">
+                                        <input type="text" name="cd_banco_ag" value="{{$item->cd_cmt_bom}}" class="form-control-sm form-control">
+                                    </td>   
+                                    <td>
+                                        <label for="cd_vr">Cartão Refeição</label>                                    
+                                        </td>
+                                        <td  colspan="">
+                                        <input type="text" name="cd_vr" value="{{$item->cd_vr}}" class="form-control-sm form-control">
+                                    </td>                      
+                                </tr> 
+                                <tr>
+                                    <td>
+                                        <label for="cd_vale_alimentacao">Vale Alimentação</label>                                    
+                                    </td>
+                                    <td  colspan="">
+                                        <input type="text" name="cd_vale_alimentacao" value="{{$item->cd_vale_alimentacao}}" class="form-control-sm form-control">
+                                    </td> 
+                                    <td>
+                                        <label for="cd_assistencia_medica_matricula">Assistencia Médica</label>                                    
+                                    </td>
+                                    <td  colspan="">
+                                        <input type="text" name="cd_assistencia_medica_matricula" value="{{$item->cd_assistencia_medica_matricula}}" class="form-control-sm form-control">
+                                    </td>
+                                    <td>
+                                        <label for="cd_assistencia_medica_matricula">SCR</label>                                    
+                                    </td>
+                                    <td  colspan="">
+                                        <select name="nr_src" class="form-control">
+                                            <option @if($item->nr_scr == 0)selected @endif value="0">Não</option>
+                                            <option @if($item->nr_scr == 1)selected @endif value="1">Sim</option>                                                                                        
+                                        </select> 
+                                    </td>   
+                                                          
+                                </tr> 
+                                <tr>
+                                    <td>
+                                        <label for="nr_hepatite_b">Hepatite B</label>                                    
+                                    </td>
+                                    <td>
+                                        <select name="nr_hepatite_b" class="form-control">
+                                            <option @if($item->nr_hepatite_b == 0)selected @endif value="0">Não Tomou</option>
+                                            <option @if($item->nr_hepatite_b == 1)selected @endif value="1">1° Dose</option>
+                                            <option @if($item->nr_hepatite_b == 2)selected @endif value="2">2° Dose</option> 
+                                            <option @if($item->nr_hepatite_b == 3)selected @endif value="3">3° Dose</option>                                              
+                                        </select> 
+                                    </td> 
+                                    <td>
+                                        <label for="nr_dupla_adulto">Dupla Adulto</label>                                    
+                                    </td>
+                                    <td >
+                                        <select name="nr_hepatite_b" class="form-control">
+                                            <option @if($item->nr_dupla_adulto == 0)selected @endif value="0">Não Tomou</option>
+                                            <option @if($item->nr_dupla_adulto == 1)selected @endif value="1">1° Dose</option>
+                                            <option @if($item->nr_dupla_adulto == 2)selected @endif value="2">2° Dose</option> 
+                                            <option @if($item->nr_dupla_adulto == 3)selected @endif value="3">3° Dose</option>                                              
+                                        </select> 
+                                    </td>
+                                    <td>
+                                        <label for="dt_dupla_adulto_validade">Validade</label>                                    
+                                    </td>
+                                    <td >
+                                        <input type="text" name="dt_dupla_adulto_validade" value="{{$item->dt_dupla_adulto_validade}}" class="form-control-sm form-control">
+                                    </td>                       
+                                </tr> 
+                                <tr>
+                                    <td>
+                                        <label for="cd_conspro">COREN</label>                                    
+                                    </td>
+                                    <td  colspan="">
+                                        <select name="cd_conspro" class="form-control">
+                                                <option @if($item->cd_conspro == 1)selected @endif value="1">Não Possui</option>
+                                                <option @if($item->cd_conspro == 2)selected @endif value="2">Possui</option>                                                                                        
+                                        </select> 
+                                    </td>  
+                                    <td>
+                                        <label for="cd_numreg">N° Coren</label>                                    
+                                    </td>
+                                    <td  colspan="">
+                                        <input type="text" name="cd_numreg" value="{{$item->cd_numreg}}" class="form-control-sm form-control">
+                                    </td>
+                                     
+                                    <td>
+                                        <label for="cd_regpro_cargo">Cargo Coren</label>                                    
+                                     </td>
+                                    <td  colspan="">
+                                        <select name="cd_rgpro_cargo" class="form-control">
+                                            <option @if($item->cd_rgpro_cargo == 0)selected @endif value="0">Não Possui</option>
+                                            <option @if($item->cd_rgpro_cargo == 1)selected @endif value="1">Auxiliar</option>
+                                            <option @if($item->cd_rgpro_cargo == 2)selected @endif value="2">Tecnico</option> 
+                                            <option @if($item->cd_rgpro_cargo == 3)selected @endif value="3">Enfermagem</option>                                              
+                                        </select>
+                                    </td>                      
+                                </tr> 
+                                <tr>
+                                    <td>
+                                        <label for="rgpro_concessao">Concessão</label>                                    
+                                    </td>
+                                    <td  colspan="">
+                                    <select name="rgpro_concessao" class="form-control">
+                                            <option @if($item->rgpro_concessao == 0)selected @endif value="0">Não Possui</option>
+                                            <option @if($item->rgpro_concessao == 1)selected @endif value="1">Definitivo</option>
+                                            <option @if($item->rgpro_concessao == 2)selected @endif value="2">Provisório</option>                                               
+                                        </select>
+                                    </td>  
+                                    <td>
+                                        <label for="cd_rgpro_status">Situação</label>                                    
+                                    </td>
+                                    <td  colspan="">
+                                        <select name="cd_rgpro_status" class="form-control">
+                                            <option @if($item->cd_rgpro_status == 0)selected @endif value="0">Não Possui</option>
+                                            <option @if($item->cd_rgpro_status == 1)selected @endif value="1">Ativo</option>
+                                            <option @if($item->cd_rgpro_status == 2)selected @endif value="2">Inativo</option>                                               
+                                        </select></td>
+                                     
+                                    <td>
+                                        <label for="obs_rgpro">Observações</label>                                    
+                                     </td>
+                                    <td  colspan="">
+                                    <input type="text" name="obs_rgpro" value="{{$item->obs_rgpro}}" class="form-control-sm form-control">
+                                    </td>                      
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <label for="dt_rgproinscr">Data Inscrição</label>                                    
+                                    </td>
+                                    <td  colspan="">
+                                        <input type="text" name="dt_rgproinscr" value="{{$item->dt_rgproinscr}}" class="form-control-sm form-control">
+                                    <td>
+                                        <label for="dt_rgpropag">Data Pagamento</label>                                    
+                                    </td>
+                                    <td  colspan="">
+                                        <input type="text" name="dt_rgpropag" value="{{$item->dt_rgpropag}}" class="form-control-sm form-control">
+                                    <td>
+                                        <label for="dt_rgproval">Validade</label>                                    
+                                     </td>
+                                    <td  colspan="">
+                                        <input type="text" name="dt_rgproval" value="{{$item->dt_rgproval}}" class="form-control-sm form-control">
+                                    </td>                      
+                                </tr> 
 
                                 </table>
                             @endforeach                            
